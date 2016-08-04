3c966140569701bc7ae2cd926ad876ac324b6a90
elfatahwashere <elfatahwashere@gmail.com>
2016-08-04 11:39:06 +0700

    Bersih bersih

--- app/src/main/java/com/example/masboyjahat/testsuitmedia/util/Constants.java
+++ app/src/main/java/com/example/masboyjahat/testsuitmedia/util/Constants.java
@@ -5,14 +5,11 @@ package com.example.masboyjahat.testsuitmedia.util;
  */
 public class Constants {
 
-    public final static String CHOOSE_EVENT = "CHOOSE_EVENT";
     public final static String PREF_NAME= "PREF_NAME";
     public final static String EVENT_NAME= "EVENT_NAME";
     public final static String GUEST_NAME= "GUEST_NAME";
     public final static String USERNAME= "USERNAME";
 
-    public final static String EVENT_LIST = "EVENT_LIST";
-    public final static String GUESTS_LIST = "GUESTS_LIST";
     public final static String GUEST_SERVER_URL = "http://dry-sierra-6832.herokuapp.com/api/people";
     public final static String EVENT_SERVER_URL = "https://raw.githubusercontent.com/elfatah/TestSuitmedia/master/app/eventDummy.json";
 
